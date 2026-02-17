245fe9d2c7e4449dc664b76d812d76b1b9ac8947
yuki <y09425717@gmail.com>
2026-02-17 14:32:38 +0900

    models/report_testのsave_mentionsテストに日報の内容が更新された場合を追加

--- test/models/report_test.rb
+++ test/models/report_test.rb
@@ -29,7 +29,8 @@ class ReportTest < ActiveSupport::TestCase
     @report.content = 'メンションテスト http://localhost:3000/reports/1 メンションテスト2 http://localhost:3000/reports/2'
     @report.save
     assert_equal(2, @report.mentioning_reports.count, '2つのメンションがある')
-    @report.destroy
+    @report.content = 'メンションテスト1、メンションテスト2'
+    @report.save
     assert_equal(0, @report.mentioning_reports.count, 'メンションが無い')
   end
 end
